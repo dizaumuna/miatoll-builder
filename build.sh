4be7960f9323a7d6e358eab7fba16c4067440d84
diza u muna <cgorkem703@outlook.com>
2026-07-17 11:23:47 +0000

    f

--- build.sh
+++ build.sh
@@ -31,9 +31,9 @@ warn () {
 sudo mv binaries/* /usr/local/bin
 
 log "Downloading given target firmware using aria2c."
-aria2c -x8 -s8 "https://gauss-compotaauto-c-cn.allawnfs.com/remove-e4ef5e6e9cb7c89e7d1c1c779fd171c1/g-10eab6008d5642cf42abd2aa41f847cb/component-ota/26/04/10/70b2e0b5874d4b06aa56e4a51bb3eba7.zip?sign=5204964c311f20d733e81c4711449ab8&t=6a5a0334&AWSAccessKeyId=ayjy7KyLVHvDqDax6_KqJgtBeORTJARg9MSGiL66&Expires=1784285746&Signature=0X2WLMQRtLcYZ1CG6RAQhQWbfac%3D" -o base.zip
+aria2c -x8 -s8 "https://gauss-compotaauto-c-cn.allawnfs.com/remove-e4ef5e6e9cb7c89e7d1c1c779fd171c1/g-10eab6008d5642cf42abd2aa41f847cb/component-ota/26/04/10/70b2e0b5874d4b06aa56e4a51bb3eba7.zip?sign=fbd5a5178fb45fe4180b8749c99f0731&t=6a5a0c03&AWSAccessKeyId=ayjy7KyLVHvDqDax6_KqJgtBeORTJARg9MSGiL66&Expires=1784287893&Signature=Lv%2FgbCh%2B16jSgy04N5fNjMOmQz8%3D" -o base.zip
 log_proc "Unzipping target firmware."
-unzip base.zip -d base_images/
+unzip base.zip payload.bin -d base_images/
 
 # run pdg
 log "Extracting images from bin file."
@@ -177,7 +177,7 @@ for partition in "${PARTITIONS[@]}"; do
     log_proc "Building $partition.img"
 
     RAW_SIZE=$(du -sb "$SRC_DIR" | cut -f1)
-    PADDED_SIZE=$(( RAW_SIZE + RAW_SIZE * 3 / 100 ))   # %3 boşluk payı
+    PADDED_SIZE=$(( RAW_SIZE + RAW_SIZE * 3 / 100 ))   # %3 padding
     BLOCK_SIZE=4096
     IMG_SIZE=$(( ( (PADDED_SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE ) * BLOCK_SIZE ))
 
