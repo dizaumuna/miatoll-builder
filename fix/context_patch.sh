058a9c69b376a07933d9ce106485dcf07dd883fe
diza u muna <cgorkem703@outlook.com>
2026-07-18 10:20:36 +0000

    Some fixes

--- fix/context_patch.sh
+++ fix/context_patch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SYS_FILE="base_img/config/system_file_contexts"
 EXT_FILE="base_img/config/system_ext_file_contexts"
-VENDOR_FILE="base_img/config/vendor_file_contexts"
+VENDOR_FILE="stock/config/vendor_file_contexts"
 
 add_line() {
     local file="$1"
@@ -19,6 +19,19 @@ add_line "$EXT_FILE" '/system_ext/priv-app/com\.qualcomm\.location(/.*)?    u:ob
 
 # fix/setup/apex/com.android.* (system/system/apex)
 add_line "$SYS_FILE" '/system/system/apex(/.*)?    u:object_r:system_file:s0'
+
+# my_ partitions
+add_line "$SYS_FILE" '/system/my_bigball(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_carrier(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_company(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_engineering(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_heytap(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_manifest(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_preload(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_product(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_region(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_reserve(/.*)?    u:object_r:system_file:s0'
+add_line "$SYS_FILE" '/system/my_stock(/.*)?    u:object_r:system_file:s0'
 
 # VNDK apex
 add_line "$EXT_FILE" '/system_ext/apex/com\.android\.vndk\.v30\.apex    u:object_r:system_file:s0'
